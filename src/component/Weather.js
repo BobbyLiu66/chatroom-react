fb7692ca8ec383b9d70e1baca0047ab9b4a44bf9
BobbyLiu66 <bobbyliu45@gmail.com>
2018-05-13 19:42:22 +1200

    add loading icon

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import axios from 'axios'
 
 import './Weather.css'
+import './Loading.css'
 
 class WeatherService extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            displayWeather:false
+            displayWeather: false
         };
         this.handleClick = this.handleClick.bind(this);
     }
@@ -16,16 +17,16 @@ class WeatherService extends Component {
         axios({
             method: 'Get',
             url: `http://13.211.150.239:3002/weather?type=current`,
-        }).then((response) =>{
+        }).then((response) => {
             this.setState(response.data);
-        }).catch((err)=>{
-            this.setState({err:err})
+        }).catch((err) => {
+            this.setState({err: err})
         })
     }
 
     handleClick(event) {
         this.setState({
-            displayWeather:!this.state.displayWeather
+            displayWeather: !this.state.displayWeather
         });
     }
 
@@ -33,28 +34,23 @@ class WeatherService extends Component {
     render() {
         return (
             <div className="weather">
-                <ul className="list-group list-group-flush weather-card">
-                    <li className="list-group-item text-center">{this.state.weather && <img className="weather-image"
-                                                                     onClick={this.handleClick}
-                                                                     src={`http://openweathermap.org/img/w/${this.state.weather[0].icon}.png`}/>}
-                    </li>
-                </ul>
-                {this.state.displayWeather &&
-                <ul className="list-group list-group-flush weather-card">
-                    <li className="list-group-item text-center"><h5>{this.state.name}</h5></li>
-                    <li className="list-group-item text-center"><p>{this.state.weather[0].description}</p></li>
-                    <li className="list-group-item text-center">
-                        <table className="text-center">
-                            <tbody>
-                            <tr >
-                                <td>{this.state.main.temp}</td>
-                                <td>{this.state.weather[0].icon}</td>
-                                <td>{this.state.main.temp}</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </li>
-                </ul>}
+
+                <div className="card text-center">
+                    {this.state.weather ? <img className="weather-image" src={`http://openweathermap.org/img/w/${this.state.weather[0].icon}.png`}
+                                               onClick={this.handleClick}/> : <div className="lds-css ng-scope loading">
+                        <div className="lds-eclipse">
+                            <div></div>
+                        </div>
+                    </div>
+
+                    }
+
+                    <div className="card-body">
+
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">body</p>
+                    </div>
+                </div>
             </div>
         );
     }
