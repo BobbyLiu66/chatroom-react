07ad36e3e2d3dffde02a0871a7451a0eaceb9e6e
BobbyLiu66 <bobbyliu45@gmail.com>
2018-05-14 21:09:17 +1200

    weather module implement

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -11,9 +11,16 @@ class WeatherService extends Component {
             displayWeather: false
         };
         this.handleClick = this.handleClick.bind(this);
+        this.getWeather = this.getWeather.bind(this);
     }
 
     componentDidMount() {
+        this.getWeather();
+        setInterval(()=>{
+        },60*60*1000);
+    }
+
+    getWeather(){
         axios({
             method: 'Get',
             url: `http://13.211.150.239:3002/weather?type=current`,
@@ -24,7 +31,7 @@ class WeatherService extends Component {
         })
     }
 
-    handleClick(event) {
+    handleClick() {
         this.setState({
             displayWeather: !this.state.displayWeather
         });
@@ -34,21 +41,37 @@ class WeatherService extends Component {
     render() {
         return (
             <div className="weather">
+                <div className="row justify-content-md-center">
+                    <div className="col-md-auto">
+                        {this.state.weather ? <img className="weather-image"
+                                                   src={`http://openweathermap.org/img/w/${this.state.weather[0].icon}.png`}
+                                                   onClick={this.handleClick}/> :
+                            <div className="lds-css ng-scope loading">
+                                <div className="lds-eclipse">
+                                    <div></div>
+                                </div>
+                            </div>}
+                            </div>
+                </div>
 
-                <div className="card text-center">
-                    {this.state.weather ? <img className="weather-image" src={`http://openweathermap.org/img/w/${this.state.weather[0].icon}.png`}
-                                               onClick={this.handleClick}/> : <div className="lds-css ng-scope loading">
-                        <div className="lds-eclipse">
-                            <div></div>
-                        </div>
-                    </div>
+                <div className="row justify-content-md-center">
+                    <div className="col-md-auto">
 
-                    }
-
-                    <div className="card-body">
-
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">body</p>
+                        {this.state.displayWeather &&
+                        <ul className="list-group list-group-flush weather-card">
+                            <li className="list-group-item text-center"><h5>{this.state.name}</h5></li>
+                            <li className="list-group-item text-center"><p>{this.state.weather[0].description}</p></li>
+                            <li className="list-group-item text-center">
+                                <table className="text-center">
+                                    <tbody>
+                                    <tr >
+                                        <td>High:{this.state.main.temp-273.15}°C</td>
+                                        <td>Low:{this.state.main.temp-273.15}°C</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </li>
+                        </ul>}
                     </div>
                 </div>
             </div>
