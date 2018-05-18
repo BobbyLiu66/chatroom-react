1db666a8bc22e48b1b7b34928d2441cb8aec2161
BobbyLiu66 <bobbyliu45@gmail.com>
2018-05-18 18:49:11 +1200

    add username display

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -15,7 +15,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-dark sticky-top bg-dark">
-                <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">USERNAME</a>
+                <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">{window.sessionStorage.username}</a>
 
                     <ul className="navbar-nav px-3">
                         <li className="nav-item text-nowrap">
