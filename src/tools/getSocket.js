572c1d99a308e4200e861306f68e935af83b0940
BobbyLiu66 <bobbyliu45@gmail.com>
2018-05-25 21:38:08 +1200

    update

--- src/tools/getSocket.js
+++ src/tools/getSocket.js
@@ -1,6 +1,5 @@
 import socketClient from 'socket.io-client'
 
-const socket = socketClient("http://localhost:3001");
-// const socket = socketClient("http://www.geekliubo.com:3001");
+const socket = socketClient("http://www.geekliubo.com:3001");
 
 export default socket
