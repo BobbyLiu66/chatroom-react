bfe82416d48128a50136cb481d664f8ff3e0f33e
BobbyLiu66 <bobbyliu45@gmail.com>
2018-05-25 16:58:42 +1200

    add weather image to weather module

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -25,7 +25,32 @@ class WeatherService extends Component {
             method: 'Get',
             url: `http://13.211.150.239:3002/weather?type=current`,
         }).then((response) => {
-            this.setState(response.data);
+            let data = response.data;
+            let img = "https://storage.googleapis.com/chatroom.geekliubo.com/weather/";
+            switch (true){
+                case (data.weather[0].description.includes('sky')):
+                    img+="sky.jpeg";
+                    break;
+                case (data.weather[0].description.includes('rain')):
+                    img+="rain.jpeg";
+                    break;
+                case (data.weather[0].description.includes('clouds')):
+                    img+="clouds.jpeg";
+                    break;
+                case (data.weather[0].description.includes('thunderstorm')):
+                    img+="thunder.jpeg";
+                    break;
+                case (data.weather[0].description.includes('snow')):
+                    img+="snow.jpeg";
+                    break;
+                case (data.weather[0].description.includes('mist')):
+                    img+="mist.jpeg";
+                    break;
+                default:
+                    break
+            }
+            data.img = img;
+            this.setState(data);
         }).catch((err) => {
             this.setState({err: err})
         })
@@ -44,7 +69,8 @@ class WeatherService extends Component {
                     <div className="col-md-auto">
                         {this.state.weather ? <img className="weather-image"
                                                    src={`http://openweathermap.org/img/w/${this.state.weather[0].icon}.png`}
-                                                   onClick={this.handleClick} alt={this.state.weather[0].description}/> :
+                                                   onClick={this.handleClick}
+                                                   alt={this.state.weather[0].description}/> :
                             <div className="lds-css ng-scope loading">
                                 <div className="lds-eclipse">
                                     <div></div>
@@ -56,21 +82,38 @@ class WeatherService extends Component {
                 <div className="row justify-content-md-center">
                     <div className="col-md-auto">
 
+                        {/*{this.state.displayWeather &&*/}
+                        {/*<div>*/}
+                            {/*<img*/}
+                                {/*src="https://storage.googleapis.com/chatroom.geekliubo.com/weather/pexels-photo-226460.jpeg"*/}
+                                {/*alt=""/>*/}
+
+                            {/*<ul className="list-group list-group-flush">*/}
+                                {/*<li className="list-group-item text-center"><h5>{this.state.name}</h5></li>*/}
+                                {/*<li className="list-group-item text-center"><p>{this.state.weather[0].description}</p>*/}
+                                {/*</li>*/}
+                                {/*<li className="list-group-item text-center">*/}
+                                    {/*<table className="text-center">*/}
+                                        {/*<tbody>*/}
+                                        {/*<tr>*/}
+                                            {/*<td>High:{this.state.main.temp - 273.15}°C</td>*/}
+                                            {/*<td>Low:{this.state.main.temp - 273.15}°C</td>*/}
+                                        {/*</tr>*/}
+                                        {/*</tbody>*/}
+                                    {/*</table>*/}
+                                {/*</li>*/}
+                            {/*</ul>*/}
+                        {/*</div>*/}
+                        {/*}*/}
                         {this.state.displayWeather &&
-                        <ul className="list-group list-group-flush weather-card">
-                            <li className="list-group-item text-center"><h5>{this.state.name}</h5></li>
-                            <li className="list-group-item text-center"><p>{this.state.weather[0].description}</p></li>
-                            <li className="list-group-item text-center">
-                                <table className="text-center">
-                                    <tbody>
-                                    <tr>
-                                        <td>High:{this.state.main.temp - 273.15}°C</td>
-                                        <td>Low:{this.state.main.temp - 273.15}°C</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </li>
-                        </ul>}
+                        <div className="card weather-card text-center">
+                            <img className="card-img-top weather-bg" src={this.state.img} alt={this.state.weather[0].description} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{this.state.name}</h5>
+                                    <p className="card-text text-muted">{this.state.weather[0].description}</p>
+                                </div>
+                        </div>
+                        }
                     </div>
                 </div>
             </div>
