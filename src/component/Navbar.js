00a330f41faad23dfd7fd71d2b62a47ad0e1ec38
BobbyLiu66 <bobbyliu45@gmail.com>
2018-06-17 16:30:52 +1200

    implement upload avatar and get response

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -30,13 +30,11 @@ class Nav extends Component {
             <nav className="navbar navbar-dark sticky-top bg-dark">
                 <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="">
                     <img
-                        src={"https://chat-picture.s3.amazonaws.com/" + window.sessionStorage.getItem('username') + ".png"}
+                        src={"https://s3.amazonaws.com/chat-picture/" + window.sessionStorage.getItem('username') + ".png"}
                         alt="" className="image-size"/>
                     {window.sessionStorage.getItem('username')}
                 </a>
 
-
-                {/*<a className="navbar-brand " href=""></a>*/}
                 <ul className="navbar-nav px-3">
                     <button type="button" className="btn btn-dark" onClick={this.handleClick}>Log out</button>
                 </ul>
--- src/page/Setting.js
+++ src/page/Setting.js
@@ -2,44 +2,59 @@ import React, {Component} from 'react';
 import socket from "../tools/getSocket";
 
 class Setting extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            imageUrl:'',
-            alertMessageStatus:false,
-            alertMessage:'',
-            photo:'',
-            fileType:''
+            imageUrl: '',
+            alertMessageStatus: false,
+            alertMessage: '',
+            photo: '',
+            fileType: ''
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this)
     }
 
-    handleChange(event){
+    handleChange(event) {
         const reader = new FileReader();
         reader.onload = function (e) {
-            this.setState({imageUrl:e.target.result})
-            }.bind(this);
+            this.setState({imageUrl: e.target.result})
+        }.bind(this);
         reader.readAsDataURL(event.target.files[0]);
-        this.setState({photo:event.target.files[0],fileType:/[^.]+$/.exec(event.target.files[0].name)[0]})
-
+        this.setState({photo: event.target.files[0], fileType: /[^.]+$/.exec(event.target.files[0].name)[0]})
     }
 
-    handleClick(){
-        if(this.state.photo){
-            socket.emit('AVATAR',{nickname:window.sessionStorage.getItem('username'),photo:this.state.photo,fileType:this.state.fileType});
-            //TODO
-            this.setState({alertMessageStatus:true,alertMessage:'Set Avatar Success'});
-        }
-        else{
-            this.setState({alertMessageStatus:true,alertMessage:'Choose your avatar'});
-        }
-        setTimeout(()=>{
-            this.setState({alertMessageStatus:false,alertMessage:''});
-        },5000)
+    componentDidMount() {
+        socket.on("AVATAR", (data) => {
+            if (data.err) {
+                this.setState({alertMessageStatus: true, alertMessage: data.err});
+            }
+            else {
+                this.setState({alertMessageStatus: true, alertMessage: 'Set Avatar Success'});
+                setTimeout(()=>{
+                    window.location.reload()
+                })
+            }
+        })
     }
 
-    render(){
+    handleClick() {
+        if (this.state.photo) {
+            socket.emit('AVATAR', {
+                nickname: window.sessionStorage.getItem('username'),
+                photo: this.state.photo,
+                fileType: this.state.fileType
+            });
+        }
+        else {
+            this.setState({alertMessageStatus: true, alertMessage: 'Choose your avatar'});
+        }
+        setTimeout(() => {
+            this.setState({alertMessageStatus: false, alertMessage: ''});
+        }, 5000)
+    }
+
+    render() {
         return (
             <div className="col-centered">
                 {this.state.alertMessageStatus &&
@@ -48,15 +63,18 @@ class Setting extends Component {
                 </div>}
                 <div className="input-group">
                     <div className="custom-file">
-                        <input type="file" accept="image/png,image/jpeg" className="custom-file-input" id="picture" onChange={this.handleChange}/>
-                            <label className="custom-file-label" htmlFor="inputGroupFile02">Set Your Avatar</label>
+                        <input type="file" accept="image/png,image/jpeg" className="custom-file-input" id="picture"
+                               onChange={this.handleChange}/>
+                        <label className="custom-file-label" htmlFor="inputGroupFile02">Set Your Avatar</label>
                     </div>
                     <div className="input-group-append">
-                        <button className="btn btn-outline-secondary" type="button" onClick={this.handleClick}>Upload</button>
+                        <button className="btn btn-outline-secondary" type="button" onClick={this.handleClick}>Upload
+                        </button>
                     </div>
                 </div>
                 <div className="input-group">
-                    {this.state.imageUrl && <img src={this.state.imageUrl} alt="" className="preview-image-size mx-auto d-block"/>}
+                    {this.state.imageUrl &&
+                    <img src={this.state.imageUrl} alt="" className="preview-image-size mx-auto d-block"/>}
                 </div>
             </div>
         )
