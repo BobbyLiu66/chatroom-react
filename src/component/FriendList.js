507d9652497c1c25cc422622d0fd307a370d03c7
BobbyLiu66 <bobbyliu45@gmail.com>
2018-07-04 20:54:30 +1200

    responsive chat area

--- src/component/FriendList.js
+++ src/component/FriendList.js
@@ -7,16 +7,16 @@ const FriendList = (props) => {
         return (
             <div className='flex-container display-area' onClick={() => props.handleClick(result)} key={result.roomName}>
                 <div className='padding'>
-                    <img src={result.imgUrl} alt=''
+                    <img src={result.imgUrl} alt={result.message.speaker}
                          className='image-size'/>
                     {!result.message.status && result.message.speaker !== window.sessionStorage.getItem('username') &&
                     <div className='circle'/>}
                 </div>
-                <div className='cols-8'>
-                    <div className='row-container'>
-                        <div className='cols-12'><p className='text-center padding'>{displayTime}</p></div>
-                        <div className='cols-12'><p
-                            className='text-truncate'>{result.message.messageContent}</p>
+                <div className='list-area'>
+                    <div className='row-container add-friend'>
+                        <div className=''><p className='text-center padding'>{displayTime}</p></div>
+                        <div className=''>
+                            <p className='text-truncate'>{result.message.messageContent}</p>
                         </div>
                     </div>
                 </div>
